74bb6e5e4de6a417e23654128d3064622f6470f6
Erin Mounts <erinys62@gmail.com>
2016-02-18 15:20:15 -0800

    Initial Commit

---
+++ SageLabMath/SageLabMath.h
@@ -0,0 +1,19 @@
+//
+//  SageLabMath.h
+//  SageLabMath
+//
+//  Created by Erin Mounts on 2/18/16.
+//  Copyright © 2016 Sage Bionetworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SageLabMath.
+FOUNDATION_EXPORT double SageLabMathVersionNumber;
+
+//! Project version string for SageLabMath.
+FOUNDATION_EXPORT const unsigned char SageLabMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SageLabMath/PublicHeader.h>
+
+
